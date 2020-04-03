e633d79af460337735515e0536209a3608b5c3f5
Sherry Shi <shi.yu@wustl.edu>
2020-04-03 14:41:10 -0500

    fix bugs in canEat method

--- Jungle/src/board/GameController.java
+++ Jungle/src/board/GameController.java
@@ -1,7 +1,7 @@
 package board;
 
 public class GameController {
-
+	private int turn;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GameBoard board = new GameBoard();
@@ -10,5 +10,9 @@ public class GameController {
 	
 	public void run() {
 		
+	}
+
+	public void win(){
+		if ()
 	}
 }
