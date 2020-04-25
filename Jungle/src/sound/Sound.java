b02b5da0f626dba62b76be56e388df1895fce82d
Yu Shi <yushi@MacBook-Pro.local>
2020-04-25 00:50:58 -0500

    add system

--- Jungle/src/sound/Sound.java
+++ Jungle/src/sound/Sound.java
@@ -1,14 +1,18 @@
 package sound;
 
+import ui.Texture;
+import utils.SystemTypeChecker;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
 public class Sound {
-    private String originalPath = "Tracks/normal.wav";
+    private static String DirPath = getPathWithSystem();
 
     public Sound(){
+        String originalPath = DirPath + "normal.wav";
         File sound = new File(originalPath);
 
         try {
@@ -40,6 +44,15 @@ public class Sound {
         }
 
     }
+
+    private static String getPathWithSystem() {
+        if(SystemTypeChecker.getType() == SystemTypeChecker.SystemType.Mac) {
+            return "Tracks/";
+        }
+        return "./Tracks/";
+    }
+
+
 
 
 }
